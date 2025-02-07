b6879454b622cdf40e692fe210ec78537ee1dc38
Pierretweenio <pierre@tweenio.com>
2025-02-07 10:09:28 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const progressBar = document.querySelector('.progress-fill');
+    const progressBar = document.querySelector('.progress-bar .progress-fill');
     if (progressBar) {
         progressBar.style.animation = 'progress 2s ease-in-out infinite';
     }
